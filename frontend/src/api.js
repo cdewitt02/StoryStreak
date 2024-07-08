7101a7d373736e6dda17d9aa88fcc3800fee4623
cdewitt02 <charliedewitt20@gmail.com>
2024-07-08 14:26:42 -0500

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-// const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
-// });
+const apiUrl = "/choreo-apis/storystreak/backend/v1";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-  });
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+//   });
   
 api.interceptors.request.use(
   (config) => {
